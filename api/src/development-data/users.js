c79123f43091ff46a447305d7670a66e93b1ab44
senjo903 <joseleonardoagreda@gmail.com>
2023-02-25 19:31:31 -0300

    readme update

---
+++ api/src/development-data/users.js
@@ -0,0 +1,16 @@
+const bcrypt = require('bcrypt');
+const { bcryptConfig } = require('./../config/index.js');
+
+const users = [
+    {
+        id: 'd706ea7d-03df-484b-975e-749e730d78b8',
+        first_name: 'Leonardo',
+        last_name: 'Agreda',
+        email: 'testing@gmail.com',
+        validate_email: true,
+        country: 'guatemala',
+        password: bcrypt.hashSync('123456', bcryptConfig.rounds)
+    }
+];
+
+module.exports = users;
