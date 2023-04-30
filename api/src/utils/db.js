8cc40e9217cd4d91bcdd08f5c99b45766a7064a7
senjo903 <joseleonardoagreda@gmail.com>
2023-04-30 04:15:58 -0300

    fix db.js

--- api/src/utils/db.js
+++ api/src/utils/db.js
@@ -1,6 +1,4 @@
 const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
 const { postgresConfig } = require('./../config/index.js');
 
 const sequelize = new Sequelize(
@@ -11,25 +9,17 @@ const sequelize = new Sequelize(
     }
 );
 
-const basename = path.basename(__filename);
+const modelDefiners = [
+    require('./../models/User'),
+    require('./../models/Role'),
+    require('./../models/Permission'),
+    require('./../models/Course'),
+    require('./../models/Postulation'),
+    require('./../models/Message'),
+    require('./../models/Template')
+];
 
-const modelDefiners = [];
-
-// We read all the files from the Models folder, require them and add them to the modelDefiners array
-fs.readdirSync(path.join(__dirname, '..', 'models'))
-    .filter((file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
-    .forEach((file) => {
-        modelDefiners.push(require(path.join(__dirname, '..', 'models', file)));
-    });
-
-// Inject the connection (sequelize) to all models
 modelDefiners.forEach((model) => model(sequelize));
-
-// We capitalize the names of the models ie: product => Product
-let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
-
-sequelize.models = Object.fromEntries(capsEntries);
 
 const { User, Role, Permission, Course, Postulation, Message } = sequelize.models;
 
