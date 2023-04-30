ac5e897d63901af95f27d7c90fd0e3a4bdfd9795
senjo903 <joseleonardoagreda@gmail.com>
2023-04-30 02:39:22 -0300

    test heroku db

--- api/src/index.js
+++ api/src/index.js
@@ -1,21 +1,18 @@
 const http = require('./http.js');
-//const { db } = require('./utils/db.js');
-//const slackApp = require('./utils/slack/appSlack');
+const { db } = require('./utils/db.js');
+const slackApp = require('./utils/slack/appSlack');
 const { serverConfig } = require('./config/index.js');
-/*
+
 db.sync({ force: serverConfig.mode })
-    .then(async () => {*/
-        /*await slackApp.start();
-        if (serverConfig.mode) {
+    .then(async () => {
+        await slackApp.start();
+        /*if (serverConfig.mode) {
             await require('./development-data/preload')();
-        }
-        */
+        }*/
         http.listen(serverConfig.port, () => {
             console.log(`Server listening on port: ${serverConfig.port}`);
         });
-        /*
     })
     .catch((error) => {
         console.log(error);
     });
-*/
