bf75b95136441a3d7ec1fca4aadc6fe64c9bf444
senjo903 <joseleonardoagreda@gmail.com>
2023-04-30 02:10:25 -0300

    fix build

--- api/src/http.js
+++ api/src/http.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const server = express();
+const path = require('path');
 const morgan = require('morgan');
 const http = require('http').createServer(server);
 const routes = require('./routes');
@@ -16,16 +17,17 @@ server.use(morgan('dev'));
 /////////////// ENDS SERVER CONFIG /////////////////////
 
 /*********** CORS CONFIG **********************/
-server.use(
-    cors({ origin: [serverConfig.client_url], credentials: true })
-);
+server.use(cors({ origin: [serverConfig.client_url], credentials: true }));
 //////////////// ENDS CORS CONFIG ///////////////////////
 
 /********** ROUTES ****************************/
+server.use(express.static('../frontend/build'));
 server.use('/api', routes);
-server.get('/hello', (req, res) => {
-    res.send('Hello World!')
-  })
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'build', 'index.html'));
+});
+//server.use(express.static(path.join(__dirname, '../../frontend/build')));
+
 ////////////////////////////////////////////////
 
 /*********** ERROR HANDLER ********************/
