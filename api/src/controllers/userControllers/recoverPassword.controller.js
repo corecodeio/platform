47673323ab2b98c625100870f910e9b06f8068b4
senjo903 <joseleonardoagreda@gmail.com>
2023-05-03 00:31:14 -0300

    fix controller recover password

--- api/src/controllers/userControllers/recoverPassword.controller.js
+++ api/src/controllers/userControllers/recoverPassword.controller.js
@@ -1,11 +1,13 @@
 const clientStytch = require('./../../utils/stytch');
-
+const { serverConfig } = require('./../../config');
 //Recover Password
 module.exports = (req, res, next) => {
     const { email } = req.body;
     clientStytch.passwords
         .resetByEmailStart({
-            email
+            email,
+            reset_password_redirect_url: `${serverConfig.client_url}/recover`,
+            login_redirect_url: `${serverConfig.client_url}/log-in`
         })
         .then(() => {
             return res.status(200).json({ successful: true, message: 'mail sent successfully' });
